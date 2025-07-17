e28eca78f01447ea2c1229af950d0885d6fc4532
becca-09 <hanauerrebeca@gmail.com>
2025-07-17 10:48:04 -0300

    Add files via upload

---
+++ padaria python.py
@@ -0,0 +1,6 @@
+nome_do_cliente= input("Nome do cliente: ")
+nome_do_produto= input("Nome do produto: ")
+preco= int(input("Preço: "))
+quantidade= int(input("Quantidade: "))
+soma = preco * quantidade
+print('O preço é: ' +str(soma))
