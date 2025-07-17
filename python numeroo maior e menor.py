ce3a0a259419827fbbb05abb244ae98f5fd04835
becca-09 <hanauerrebeca@gmail.com>
2025-07-17 08:24:42 -0300

    Add files via upload

---
+++ python numeroo maior e menor.py
@@ -0,0 +1,14 @@
+print("=== Comparar dois numeros ==")
+# Ler dois numeros da tela
+a = int(input("Digite um numero:"))
+b = int(input("Digite um numero:"))
+#Comparar os dois numeros
+if(a == b):
+    print("Numero iguas!")
+else:
+    print("Numeros diferentes!")
+#Mostrar o numero maior
+if (a > b):
+    print("O menosr é a =",a)
+else:
+    print("O menos é b =",b)
